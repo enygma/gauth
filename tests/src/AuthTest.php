abbb58b47c27028453ab84ff9a8d2322f6fa4899
Matt Parker <matt@lamplightdb.co.uk>
2015-01-19 22:39:43 +0000

    Tests for generating a code

--- tests/src/AuthTest.php
+++ tests/src/AuthTest.php
@@ -26,40 +26,8 @@ class AuthTest extends \PHPUnit_Framework_TestCase {
 
 	public function test_base64_lookup_is_correct () {
 
-		$expected = array(
-            'A' => 0,
-            'B' => 1,
-            'C' => 2,
-            'D' => 3,
-            'E' => 4,
-		    'F' => 5,
-		    'G' => 6,
-		    'H' => 7,
-		    'I' => 8,
-		    'J' => 9,
-		    'K' => 10,
-		    'L' => 11,
-		    'M' => 12,
-		    'N' => 13,
-		    'O' => 14,
-		    'P' => 15,
-		    'Q' => 16,
-		    'R' => 17,
-		    'S' => 18,
-		    'T' => 19,
-		    'U' => 20,
-		    'V' => 21,
-		    'W' => 22,
-		    'X' => 23,
-		    'Y' => 24,
-		    'Z' => 25,
-		    2 => 26,
-		    3 => 27,
-		    4 => 28,
-		    5 => 29,
-		    6 => 30,
-		    7 => 31,
-        );
+		$expected = $this->base64values;
+
         $auth = new Auth();
         $this->assertEquals($expected, $auth->getLookup());
 	}
@@ -100,5 +68,63 @@ class AuthTest extends \PHPUnit_Framework_TestCase {
 
 	}
 
-	
+
+	public function test_set_and_get_code_length () {
+
+		$length = 123;
+		$auth = new Auth();
+		$auth->setCodeLength($length);
+		$this->assertEquals($length, $auth->getCodeLength());
+	}
+
+
+	public function test_generate_a_init_code () {
+
+		$auth = new Auth();
+		$code = $auth->generateCode();
+		$this->assertEquals(16, strlen($code), 'Default code is 16 chars');
+
+		$base64values = array_keys($this->base64values);
+		$code_letters = str_split($code);
+
+		foreach ($code_letters as $letter) {
+			$this->assertContains($letter, $base64values, 'Key consists of base 64 values');
+		}
+	}
+
+
+	private $base64values = array(
+        'A' => 0,
+        'B' => 1,
+        'C' => 2,
+        'D' => 3,
+        'E' => 4,
+	    'F' => 5,
+	    'G' => 6,
+	    'H' => 7,
+	    'I' => 8,
+	    'J' => 9,
+	    'K' => 10,
+	    'L' => 11,
+	    'M' => 12,
+	    'N' => 13,
+	    'O' => 14,
+	    'P' => 15,
+	    'Q' => 16,
+	    'R' => 17,
+	    'S' => 18,
+	    'T' => 19,
+	    'U' => 20,
+	    'V' => 21,
+	    'W' => 22,
+	    'X' => 23,
+	    'Y' => 24,
+	    'Z' => 25,
+	    2 => 26,
+	    3 => 27,
+	    4 => 28,
+	    5 => 29,
+	    6 => 30,
+	    7 => 31,
+    );
 }
